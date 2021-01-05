e2e98e8c414fb9b237b9d6137bf729edd2152ddf
龙鸣 <1653219895@qq.com>
2021-01-05 16:01:27 +0800

    构建依赖

---
+++ eurekaserver/src/main/java/com/alsa/Application.java
@@ -0,0 +1,14 @@
+package com.alsa;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Created with IDEA
+ * @author:longming
+ * @Date: 2021/1/5
+ * @Time: 15:58
+ * @Description:
+ */
+@SpringBootApplication
+public class Application {
+}
